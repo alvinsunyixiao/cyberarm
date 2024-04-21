2aded438e58e83760ef5e8f7dc7b9f854e4ea1ba
Alvin Sun <alvinsunyixiao@gmail.com>
2024-04-21 16:59:36 -0700

    separate launch file

---
+++ launch/bringup.launch.py
@@ -0,0 +1,23 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import PathJoinSubstitution
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    urdf_launch_package = FindPackageShare('urdf_launch')
+
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PathJoinSubstitution([urdf_launch_package, 'launch', 'description.launch.py']),
+            launch_arguments={
+                'urdf_package': 'cyberarm',
+                'urdf_package_path': 'models/model.urdf'}.items()
+        ),
+        Node(
+            executable="control.py",
+            package="cyberarm",
+        ),
+    ])
