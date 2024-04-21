e3530d91e06e20bb34665655a6bb962116bffd07
Alvin Sun <alvinsunyixiao@gmail.com>
2024-04-21 15:16:51 -0700

    visualization working

---
+++ launch/viz.launch.py
@@ -0,0 +1,34 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    urdf_launch_package = FindPackageShare('urdf_launch')
+
+    default_rviz_config_path = PathJoinSubstitution([urdf_launch_package, 'config', 'urdf.rviz'])
+    ld.add_action(DeclareLaunchArgument(name='rviz_config', default_value=default_rviz_config_path,
+                                        description='Absolute path to rviz config file'))
+
+    # need to manually pass configuration in because of https://github.com/ros2/launch/issues/313
+    ld.add_action(IncludeLaunchDescription(
+        PathJoinSubstitution([urdf_launch_package, 'launch', 'description.launch.py']),
+        launch_arguments={
+            'urdf_package': 'cyberarm',
+            'urdf_package_path': 'models/cyberarm/model.urdf'}.items()
+    ))
+
+    ld.add_action(Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen',
+        arguments=['-d', LaunchConfiguration('rviz_config')],
+    ))
+    return ld
