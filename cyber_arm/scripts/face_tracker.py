ec8e62bd4dde7bf0100a82382644da36705d2e8e
Alvin Sun <alvinsunyixiao@gmail.com>
2024-05-06 16:31:05 -0700

    add config init to face tracker

--- cyber_arm/scripts/face_tracker.py
+++ cyber_arm/scripts/face_tracker.py
@@ -2,6 +2,7 @@
 
 import cv2
 import dlib
+import math
 
 import rclpy
 from rclpy.node import Node
@@ -9,15 +10,29 @@ from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 
+from cyber_msgs.msg import CyberarmConfig
+
 
 class FaceDetectorNode(Node):
     def __init__(self):
         super().__init__("face_detector_node")
+
         self.img_sub = self.create_subscription(Image, "/camera/camera/color/image_raw", self.img_callback, 10)
         self.viz_pub = self.create_publisher(Image, "/viz/detected_faces", 10)
         self.bridge = CvBridge()
         self.detector = dlib.get_frontal_face_detector()
         self.predictor = dlib.shape_predictor("../ml/shape_predictor_5_face_landmarks.dat")
+
+        self.send_init_config()
+
+    def send_init_config(self):
+        self.config_pub = self.create_publisher(CyberarmConfig, "/ctrl/configuration", 10)
+        config = CyberarmConfig()
+        config.q[0] = math.radians(-90.)
+        config.q[1] = math.radians(30.)
+        config.q[2] = math.radians(-60.)
+        config.q[3] = 0.
+        self.config_pub.publish(config)
 
     def img_callback(self, msg: Image):
         img = self.bridge.imgmsg_to_cv2(msg, desired_encoding="rgb8")
