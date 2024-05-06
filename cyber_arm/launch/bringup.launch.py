68c514f59e331c98ffe49b1fe34852560fcf2510
Alvin Sun <alvinsunyixiao@gmail.com>
2024-05-06 14:51:50 -0700

    move camera transform to a static publisher

--- cyber_arm/launch/bringup.launch.py
+++ cyber_arm/launch/bringup.launch.py
@@ -1,15 +1,24 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.substitutions import PathJoinSubstitution
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.conditions import IfCondition
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
 
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
+    camera = LaunchConfiguration("camera", default="false")
+
     urdf_launch_package = FindPackageShare('urdf_launch')
 
     return LaunchDescription([
+        DeclareLaunchArgument(
+            "camera",
+            default_value=camera,
+            choices=["true", "false"],
+            description="set to true to bringup D435i camera",
+        ),
         IncludeLaunchDescription(
             PathJoinSubstitution([urdf_launch_package, 'launch', 'description.launch.py']),
             launch_arguments={
@@ -19,5 +28,9 @@ def generate_launch_description():
         Node(
             executable="cyberarm_node",
             package="cyber_arm",
+        ),
+        IncludeLaunchDescription(
+            PathJoinSubstitution([FindPackageShare("cyber_arm"), "launch", "camera.launch.py"]),
+            condition=IfCondition(camera),
         ),
     ])
---
+++ cyber_arm/launch/camera.launch.py
@@ -0,0 +1,37 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import PathJoinSubstitution
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PathJoinSubstitution([FindPackageShare("realsense2_camera"), "launch", "rs_launch.py"]),
+            launch_arguments={
+                "enable_color": "true",
+                "enable_depth": "true",
+                "enable_rgbd": "true",
+                "enable_sync": "true",
+                "align_depth.enable": "true",
+                "rgb_camera.color_profile": "640x480x30",
+                "depth_module.depth_profile": "640x480x30",
+            }.items(),
+        ),
+        Node(
+            package="tf2_ros",
+            executable="static_transform_publisher",
+            arguments=[
+                "--x", "-0.056",
+                "--y", "0",
+                "--z", "0.10095",
+                "--roll", "0",
+                "--pitch", "-1.5708",
+                "--yaw", "1.5708",
+                "--frame-id", "end_effector_link",
+                "--child-frame-id", "camera_link",
+            ],
+        ),
+    ])
